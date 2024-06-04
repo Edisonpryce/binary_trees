0b535c71be67f9bde77a6d0f7bad98c172023bcc
edison pryce <edisonpryce@gmail.com>
2024-06-04 14:35:32 -0700

    new

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,9 +14,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
     {
         return NULL;
     }
-    new_node->right = parent->right;
     if (parent->right != NULL)
     {
+	new_node->right = parent->right;
         parent->right->parent = new_node;
     }
     parent->right = new_node;
