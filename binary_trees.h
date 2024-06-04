1b5e4a7a015ee16f5d3e5ba177079989105e904b
edison pryce <edisonpryce@gmail.com>
2024-06-04 07:01:09 -0700

    new

--- binary_trees.h
+++ binary_trees.h
@@ -20,7 +20,6 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
@@ -28,5 +27,6 @@ typedef struct binary_tree_s heap_t;
 
 /* --------- functional prototype --------- */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
