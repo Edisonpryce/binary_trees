2ff5f1dc1a8269684e2dd1c7f6bf260d7e5b5f03
edison pryce <edisonpryce@gmail.com>
2024-06-04 06:52:43 -0700

    new

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,10 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,5 +22,8 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+/* --------- functional prototype --------- */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
