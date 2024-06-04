4bc840b95e7191e882bcfd648b706831300343a2
edison pryce <edisonpryce@gmail.com>
2024-06-04 14:48:50 -0700

    new

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,7 +10,13 @@ void binary_tree_delete(binary_tree_t *tree) {
     {
 	    return;
     }
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
-    free(tree);
+    else
+    {
+	    if (tree != NULL)
+	    {
+    		binary_tree_delete(tree->left);
+    		binary_tree_delete(tree->right);
+	    }
+	    free(tree);
+    }
 }
