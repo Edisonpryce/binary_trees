4f5ab403752272878498115faa1fd2865870ae6c
salehgomaa60 <salehgomaazzb@gmail.com>
2024-06-05 15:57:06 -0700

    fixing task 3 and removing if else statement

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,18 +6,12 @@
  * Return: Nothing
  */
 
-void binary_tree_delete(binary_tree_t *tree) {
-    if (tree == NULL)
-    {
-	    return;
-    }
-    else
-    {
-	    if (tree != NULL)
-	    {
-    		binary_tree_delete(tree->left);
-    		binary_tree_delete(tree->right);
-	    }
-	    free(tree);
-    }
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
 }
